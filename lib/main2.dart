f0d6e2eb96363a2a57e25086e5a1538718489b42
jagriti <im.jagriti.jha@gmail.com>
2024-02-21 22:43:19 +0530

    basic flutter screen

--- lib/main2.dart
+++ lib/main2.dart
@@ -6,7 +6,15 @@ void main() {
         child: Text(
       'Hello\nDart\nFlutter',
       textDirection: TextDirection.ltr,
-      style: TextStyle(color: Colors.red.shade900, height: 8, fontWeight: FontWeight.bold, fontSize: 28, decoration: TextDecoration.underline, decorationColor: Colors.amber, decorationThickness: 2),
+      style: TextStyle(
+          color: Colors.red.shade900,
+          height: 8,
+
+          fontWeight: FontWeight.bold,
+          fontSize: 28,
+          decoration: TextDecoration.underline,
+          decorationColor: Colors.amber,
+          decorationThickness: 2),
     )),
   );
 }
