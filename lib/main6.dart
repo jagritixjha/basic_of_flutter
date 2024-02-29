1da4720c18f18eaf796fbff2fde2156513ec650c
jagriti <im.jagriti.jha@gmail.com>
2024-02-29 14:14:40 +0530

    new commit

---
+++ lib/main6.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen,
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen,
+        elevation: 6,
+        shadowColor: Colors.green,
+        title: const Text(
+          textDirection: TextDirection.ltr,
+          'My App',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            color: Colors.lightGreenAccent,
+            border: Border.all(color: Colors.green, width: 20),
+          ),
+          child: const Center(
+            child: Text(
+              'OOOO',
+              style: TextStyle(
+                color: Colors.black45,
+                letterSpacing: -28,
+                fontSize: 85,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
