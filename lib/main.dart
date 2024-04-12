fc0d83446e5a0c8c9e0e6263ee0dbb091754ffe0
jagriti <im.jagriti.jha@gmail.com>
2024-04-12 22:58:34 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
-import 'package:app_bar/textfield_widget.dart';
+import 'package:app_bar/list_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TextFieldWidgets(),
+      home: ListViewScreen(),
     ),
   );
 }
