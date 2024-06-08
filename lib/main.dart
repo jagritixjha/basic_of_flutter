4e22e6741b4c197f0fe050ace7a456beb0a6f842
jagriti <im.jagriti.jha@gmail.com>
2024-06-08 23:47:17 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:app_bar/list_view.dart';
+import 'package:app_bar/grid_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ListViewScreen(),
+      home: GridViewScreen(),
     ),
   );
 }
